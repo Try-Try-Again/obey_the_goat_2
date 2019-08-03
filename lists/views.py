b150430eb147a3eb3dbb585fde5a43462daa456f
Your Name <you@example.com>
2019-08-03 16:28:08 -0700

    FT still breaking early. Will compare at end of chapter

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
