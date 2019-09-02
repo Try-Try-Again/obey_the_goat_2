c37ca370b5f9bea132ce16d3d67a9da63ac480a7
Your Name <you@example.com>
2019-09-01 23:46:07 -0700

    end of chapter 15 and errthang works

--- lists/forms.py
+++ lists/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = "Ya can't have duplicate items ya dingus!"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -37,3 +37,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
